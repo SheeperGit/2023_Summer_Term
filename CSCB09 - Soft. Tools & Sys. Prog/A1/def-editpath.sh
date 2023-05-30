afabe419cca6a8e722bfcde2402df1c76e4eee3b
SheeperGit <sean1900.s@gmail.com>
2023-05-30 19:37:50 -0400

    adpExamples.txt

--- CSCB09 - Soft. Tools & Sys. Prog/A1/def-editpath.sh
+++ CSCB09 - Soft. Tools & Sys. Prog/A1/def-editpath.sh
@@ -16,7 +16,7 @@ editpath() {
             d)
                 action="delete"
                 ;;
-            \?) ## Unknown Option - Exit Code (1) ##
+            \?) # Unknown Option - Exit Code (1)
                 echo "Invalid option: -$OPTARG" >&2
                 return 1
                 ;;
@@ -26,40 +26,42 @@ editpath() {
     ## Shift according to the # of opts. ##
     shift $(($OPTIND - 1))
 
-    # '--' Check #
+    ## "--" Check ##
     separator_index=-1
-    for i in $(seq 1 $#); do
-        if [ "${!i}" = "--" ]; then
+    i=1
+    for arg in "$@"; do
+        if [ "$arg" = "--" ]; then
             separator_index=$i
             break
         fi
+        i=$((i + 1))
     done
 
-    # Separate opts and paths #
+    ## Separate opts and paths ##
     if [ $separator_index -ne -1 ]; then
         paths="${@:$((separator_index + 1))}"
     else
         paths="$@"
     fi
 
-    # Perform action on PATH #
+    ## Perform action on PATH ##
     case $action in
         "append")
-            for path in "${paths}"; do
-                PATH="${PATH}${delimiter}"${path}""
+            for path in "$paths"; do
+                PATH="${PATH}${delimiter}${path}"
             done
             ;;
         "prepend")
-            for path in $paths; do
-                PATH=""${path}"${delimiter}${PATH}"
+            for path in "$paths"; do
+                PATH="${path}${delimiter}${PATH}"
             done
             ;;
         "delete")
-            for path in $paths; do
-                PATH=$(echo "$PATH" | awk -v RS="$delimiter" -v ORS="$delimiter" '!/'"$path"'/')
+            for path in "$paths"; do
+                PATH=$(echo "$PATH" | tr $delimiter '\n' | grep -vFx "$path" | tr '\n' $delimiter)
             done
             ;;
-        *) ## Unknown Action - Exit Code (2) ##
+        *) # Unknown Action - Exit Code (2)
             echo "Invalid action: $action" >&2
             return 2
             ;;
@@ -67,4 +69,3 @@ editpath() {
 
     export PATH
 }
-
