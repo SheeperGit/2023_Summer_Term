b834b86c1c3ebb41a88af6560ad6ecf73997cea6
SheeperGit <sean1900.s@gmail.com>
2023-05-31 23:22:31 -0400

    working transpose.sh

--- CSCB09 - Soft. Tools & Sys. Prog/Labs/Lab03/transpose.sh
+++ CSCB09 - Soft. Tools & Sys. Prog/Labs/Lab03/transpose.sh
@@ -5,9 +5,18 @@ if [ $# -ne 2 ] ; then
     return 1
 fi
 
-if [ $(mkdir "$2"; echo $?) -ne 0 ] ; then
+if [ -d "$2" ] ; then
     echo Failed to create "$2". >&2
     return 2
 fi
 
-cp -R "$1" "$2"
+mkdir "$2"
+for i in "$1"/* ; do
+    firstlvl=$(basename "$i")
+    for j in "$i"/* ; do  
+        secondlvl=$(basename "$j")
+        mkdir "$2/${secondlvl}/"
+        mkdir "$2/${secondlvl}/${firstlvl}"
+        cp -nr "$j"/* "$2/${secondlvl}/${firstlvl}" # -n and -r combined!
+    done
+done
