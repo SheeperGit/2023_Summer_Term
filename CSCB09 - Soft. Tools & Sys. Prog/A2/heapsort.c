7c52a027a5ee3b4dd48f97c855ce8b6bc8089a94
SheeperGit <sean1900.s@gmail.com>
2023-06-18 22:20:12 -0400

    First take of numofRecords

--- CSCB09 - Soft. Tools & Sys. Prog/A2/heapsort.c
+++ CSCB09 - Soft. Tools & Sys. Prog/A2/heapsort.c
@@ -1,5 +1,31 @@
 #include "heapsort.h"
 #include "customer.h"
+#include <stdio.h> // DELETE LATER
 
 int heapsort(const char *filename) {
+    FILE *f;
+    if ((f = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "Unable to read from file\n");
+    return 0;
+    }
+
+    fseek(f, 48, 0);
+    int numofRecords = ftell(f) / sizeof(customer);
+    printf("numofRecords = %d\n", numofRecords);
+
+    fclose(f);
+    return 1;
 }
+
+int main(void){
+    char *myfilename = "sample.dat";
+    int result = heapsort(myfilename);
+    return 0;
+}
+
+// My debug/test cases: //
+#ifndef EBUGFLAG
+#define EBUGFLAG
+
+
+#endif
