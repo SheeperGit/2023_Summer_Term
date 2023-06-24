81ade5fc83f154fc6f03e3d31fdb8cae0a70b903
SheeperGit <sean1900.s@gmail.com>
2023-06-24 11:19:01 -0400

    [heapsort.c w/ -O2 flag fixed] heapsort.c (pv2)

--- CSCB09 - Soft. Tools & Sys. Prog/A2/heapsort.c
+++ CSCB09 - Soft. Tools & Sys. Prog/A2/heapsort.c
@@ -4,79 +4,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-// I won't bother checking whether delimiting is needed. //
-// The additional if statement seems like more computation //
-// and so I'm choosing to ignore this check. I think this is right? //
-
 void heapify_at_v(FILE *f, int n, int i) {
     int largest = i;
     int left_index = (2 * i) + 1;
     int right_index = (2 * i) + 2;
 
-    char curCustName[CUSTOMER_NAME_MAX + 1];
-    char largestCustName[CUSTOMER_NAME_MAX + 1];
-    unsigned curCustLoyalty;
-    unsigned largestCustLoyalty;
-
     // Read the records at given indices. //
-    fseek(f, i * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-    fread(&curCustName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-    curCustName[CUSTOMER_NAME_MAX - 1] = '\0';
-    fread(&curCustLoyalty, sizeof(unsigned), 1, f);
+    customer curCust, largestCust;
+    fseek(f, i * sizeof(customer), SEEK_SET);
+    fread(&curCust, sizeof(customer), 1, f);
 
-    fseek(f, largest * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-    fread(&largestCustName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-    largestCustName[CUSTOMER_NAME_MAX - 1] = '\0';
-    fread(&largestCustLoyalty, sizeof(unsigned), 1, f);
-    
+    fseek(f, largest * sizeof(customer), SEEK_SET);
+    fread(&largestCust, sizeof(customer), 1, f);
 
     // Compare loyalty, and if same: sort alphabetically! // 
     if (left_index < n) {
-        char leftCustName[CUSTOMER_NAME_MAX + 1];
-        unsigned leftCustLoyalty;
+        customer leftCust;
+        fseek(f, left_index * sizeof(customer), SEEK_SET);
+        fread(&leftCust, sizeof(customer), 1, f);
 
-        fseek(f, left_index * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-        fread(&leftCustName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-        leftCustName[CUSTOMER_NAME_MAX - 1] = '\0';
-        fread(&leftCustLoyalty, sizeof(unsigned), 1, f);
-    
-        if (leftCustLoyalty > largestCustLoyalty || 
-            (leftCustLoyalty == largestCustLoyalty && strcmp(leftCustName, largestCustName) > 0))
+        if (leftCust.loyalty > largestCust.loyalty || 
+            (leftCust.loyalty == largestCust.loyalty && strcmp(leftCust.name, largestCust.name) > 0))
         {
             largest = left_index;
-            strcpy(largestCustName, leftCustName);
-            largestCustLoyalty = leftCustLoyalty;
+            largestCust = leftCust;
         }
     }
 
     if (right_index < n) {
-        char rightCustName[CUSTOMER_NAME_MAX + 1];
-        unsigned rightCustLoyalty;
-
-        fseek(f, right_index * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-        fread(&rightCustName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-        rightCustName[CUSTOMER_NAME_MAX - 1] = '\0';
-        fread(&rightCustLoyalty, sizeof(unsigned), 1, f);
-
-        if (rightCustLoyalty > largestCustLoyalty || 
-            (rightCustLoyalty == largestCustLoyalty && strcmp(rightCustName, largestCustName) > 0))
+        customer rightCust;
+        fseek(f, right_index * sizeof(customer), SEEK_SET);
+        fread(&rightCust, sizeof(customer), 1, f);
+            
+        if (rightCust.loyalty > largestCust.loyalty ||
+            (rightCust.loyalty == largestCust.loyalty && strcmp(rightCust.name, largestCust.name) > 0))
         {
             largest = right_index;
-            strcpy(largestCustName, rightCustName);
-            largestCustLoyalty = rightCustLoyalty;
+            largestCust = rightCust;
         }
     }
 
     // Swap records (if necessary) //
     if (largest != i) {
         // Write records at given indices //
-        fseek(f, i * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-        fwrite(&largestCustName, sizeof(CUSTOMER_NAME_MAX), 1, f);
-        fwrite(&largestCustLoyalty, sizeof(unsigned), 1, f);
-
-        fseek(f, largest * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-        fwrite(&curCustName, sizeof(CUSTOMER_NAME_MAX), 1, f);
-        fwrite(&curCustLoyalty, sizeof(unsigned), 1, f);
+        fseek(f, i * sizeof(customer), SEEK_SET);
+        fwrite(&largestCust, sizeof(customer), 1, f);
+        fseek(f, largest * sizeof(customer), SEEK_SET);
+        fwrite(&curCust, sizeof(customer), 1, f);
 
         // Recursively heapify the affected subtree //
         heapify_at_v(f, n, largest);
@@ -103,29 +77,18 @@ int heapsort(const char *filename) {
 
         // Swap root (max element) with the last record //
         fseek(f, 0, SEEK_SET);
-        char rootName[CUSTOMER_NAME_MAX + 1];
-        char lastName[CUSTOMER_NAME_MAX + 1];
-        unsigned rootLoyalty;
-        unsigned lastLoyalty;
+        customer root;
+        fread(&root, sizeof(customer), 1, f);
 
-        fread(&rootName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-        rootName[CUSTOMER_NAME_MAX - 1] = '\0';
-        fread(&rootLoyalty, sizeof(unsigned), 1, f);
-        
         fseek(f, i * sizeof(customer), SEEK_SET);
+        customer last;
+        fread(&last, sizeof(customer), 1, f);
 
-        fread(&lastName, sizeof(CUSTOMER_NAME_MAX) + 1, 1, f);
-        lastName[CUSTOMER_NAME_MAX - 1] = '\0';
-        fread(&lastLoyalty, sizeof(unsigned), 1, f);
-        
-
-        fseek(f, i * sizeof(CUSTOMER_NAME_MAX), SEEK_SET);
-        fwrite(&rootName, sizeof(CUSTOMER_NAME_MAX), 1, f);
-        fwrite(&rootLoyalty, sizeof(unsigned), 1, f);
+        fseek(f, i * sizeof(customer), SEEK_SET);
+        fwrite(&root, sizeof(customer), 1, f);
 
         fseek(f, 0, SEEK_SET);
-        fwrite(&lastName, sizeof(CUSTOMER_NAME_MAX), 1, f);
-        fwrite(&lastLoyalty, sizeof(unsigned), 1, f);
+        fwrite(&last, sizeof(customer), 1, f);
 
         // Reduce heap size and heapify root //
         heapify_at_v(f, i, 0);
@@ -156,12 +119,9 @@ int main(void){
     fseek(f, 0, SEEK_SET);
     printf("Sorted records:\n");
     for (int i = 0; i < numRecords; i++) {
-        char curName[CUSTOMER_NAME_MAX + 1];
-        unsigned curLoyalty;
-        fread(&curName, sizeof(CUSTOMER_NAME_MAX), 1, f);
-        fread(&curLoyalty, sizeof(unsigned), 1, f);
-
-        printf("Name: %s\tLoyalty: %u\n", curName, curLoyalty);
+        customer current;
+        fread(&current, sizeof(customer), 1, f);
+        printf("Name: %s\tLoyalty: %u\n", current.name, current.loyalty);
     }
 
     return 0;
