219295d5fd297a8f1d6363aff7409f9d56e65ecf
SheeperGit <sean1900.s@gmail.com>
2023-07-18 20:34:33 -0400

    b09 lab10 working

--- CSCB09 - Soft. Tools & Sys. Prog/Labs/Lab09/bridge.c
+++ CSCB09 - Soft. Tools & Sys. Prog/Labs/Lab09/bridge.c
@@ -3,74 +3,69 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main() {
-    // FDs for foo, bar //
-    int foo2bar[2]; // Pipe for foo->bar comms //
-    int bar2foo[2]; // Pipe for bar->foo comms //
-    
-    if (pipe(foo2bar) < 0 || pipe(bar2foo) < 0) {
-        perror("Failed to create pipes");
-        exit(1);
+#define BUFSIZE 80
+
+int main(){
+    int foo2bar[2];
+    int bar2foo[2];
+
+    if(pipe(foo2bar) == -1 || pipe(bar2foo) == -1){
+        perror("Failed to creates pipe(s)!");
+        return 2;
     }
 
-    pid_t fooPID, barPID;
-    fooPID = fork();
+    pid_t fooPID;
+    if((fooPID = fork()) == -1){
+        perror("Failed to fork!");
+        return 3;
+    }
 
-    if (fooPID == -1) {
-        perror("Failed to fork foo");
-        exit(1);
-    } 
-    
-    else if (fooPID == 0) {
-        // Child process (foo) //
-        close(foo2bar[0]); // Close unused read-end //
-        close(bar2foo[1]); // Close unused write-end //
+    if(fooPID != 0){
+        pid_t barPID;
+        if((barPID = fork()) == -1){
+            perror("Failed to fork!");
+            return 3;
+        }
 
-        dup2(bar2foo[0], 0); // Set stdin to read-end of bar2foo //
-        dup2(foo2bar[1], 1); // Set stdout to write-end of foo2bar //
+        if(barPID != 0){
+            // Parent Process (bridge) //
+            close(bar2foo[1]);
+            close(foo2bar[0]);
+            close(bar2foo[0]);
+            close(foo2bar[1]);
+            wait(NULL);
+        }
 
-        // Execute foo //
-        execl("./foo", "foo", "1", "2", NULL);
+        else{
+            // Child Process (foo) //
+            close(bar2foo[1]);
+            close(foo2bar[0]);
 
-        perror("Failed to exec foo");
-        exit(1);
-    } 
-    
-    else {
-        barPID = fork();
-
-        if (barPID == -1) {
-            perror("Failed to fork bar");
+            char in[BUFSIZE];
+            char out[BUFSIZE];
+            
+            snprintf(in, BUFSIZE, "%d", bar2foo[0]);
+            snprintf(out, BUFSIZE, "%d", foo2bar[1]);
+            
+            execlp("./foo", "./foo", in, out, (char *)NULL);
+            perror("Failed to exec!");
             exit(1);
-        } 
-        
-        else if (barPID == 0) {
-            // Child process (bar) //
-            close(foo2bar[1]); // Close unused write-end //
-            close(bar2foo[0]); // Close unused read-end //
-
-            dup2(foo2bar[0], 0); // Set stdin to read-end of foo2bar //
-            dup2(bar2foo[1], 1); // Set stdout to write-end of bar2foo //
-
-            // Execute bar //
-            execl("./bar", "bar", "1", "2", NULL);
-
-            perror("Failed to exec bar");
-            exit(1);
-        } 
-        
-        else {
-            // Parent process (bridge) //
-            close(foo2bar[0]); // Close unused read-end //
-            close(foo2bar[1]); // Close unused write-end //
-            close(bar2foo[0]); // Close unused read-end //
-            close(bar2foo[1]); // Close unused write-end //
-
-            // Wait for both foo, bar to terminate //
-            waitpid(fooPID, NULL, 0);
-            waitpid(barPID, NULL, 0);
         }
     }
+    
+    else{
+        // Child process (bar) //
+        close(bar2foo[0]);
+        close(foo2bar[1]);
 
-    return 0;
+        char in[BUFSIZE];
+        char out[BUFSIZE];
+            
+        snprintf(in, BUFSIZE, "%d", foo2bar[0]);
+        snprintf(out, BUFSIZE, "%d", bar2foo[1]);
+
+        execlp("./bar", "./bar", in, out, (char *)NULL);
+        perror("Failed to exec!");
+        exit(1);
+    }
 }
