581fed4b3397f1c18867528cdecf78e5f7ca8336
SheeperGit <sean1900.s@gmail.com>
2023-08-01 20:19:52 -0400

    b09 A4 skeleton

--- CSCB09 - Soft. Tools & Sys. Prog/A4/hmu-client.c
+++ CSCB09 - Soft. Tools & Sys. Prog/A4/hmu-client.c
@@ -11,8 +11,92 @@
 #include <string.h>
 #include <unistd.h>
 
-// cmdline reminder: IP-address, portnum, username, filename
-int main(int argc, char *argv[]) {
-  // TODO
-  return 0;
+#define MAX_BUFFER_SIZE 1024
+
+void send_data(int sockfd, const char *data) {
+    if (send(sockfd, data, strlen(data), 0) == -1) {
+      perror("Error sending data");
+      exit(EXIT_FAILURE);
+    }
 }
+
+// cmdline reminder: IP-address, portnum, username, filename //
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+      fprintf(stderr, "Usage: %s <server_address> <server_port> <username> <filename>\n", argv[0]);
+      exit(EXIT_FAILURE);
+    }
+
+    const char *server_addr = argv[1];
+    int server_port = atoi(argv[2]);
+    const char *username = argv[3];
+    const char *filename = argv[4];
+
+    // Open the file to read its content and determine its size
+    FILE *f = fopen(filename, "r");
+    if (!f) {
+      fprintf(stderr, "Error opening file: %s\n", filename);
+      exit(EXIT_FAILURE);
+    }
+
+    fseek(f, 0, SEEK_END);
+    long file_size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    // Create a buffer to store the entire file content
+    char file_content[MAX_BUFFER_SIZE];
+    size_t bytes_read = fread(file_content, 1, sizeof(file_content), f);
+    fclose(f);
+
+    if (bytes_read != file_size) {
+      fprintf(stderr, "Error reading file: %s\n", filename);
+      exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server //
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+      perror("Error opening socket");
+      exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    inet_pton(AF_INET, server_addr, &server_addr.sin_addr);
+
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+      perror("Error connecting to the server");
+      exit(EXIT_FAILURE);
+    }
+
+    // Send user name, file name, file size, and file content to the server //
+    send_data(sockfd, username);
+    send_data(sockfd, "\n");
+
+    send_data(sockfd, filename);
+    send_data(sockfd, "\n");
+
+    char file_size_str[11]; // 10 digits for file size + '\0'
+    sprintf(file_size_str, "%ld", file_size);
+    send_data(sockfd, file_size_str);
+    send_data(sockfd, "\n");
+
+    send_data(sockfd, file_content);
+
+    // Receive serial number from the server //
+    char serial_number_str[11]; // 10 digits for serial number + '\0'
+    int recv_bytes = recv(sockfd, serial_number_str, sizeof(serial_number_str) - 1, 0);
+    if (recv_bytes <= 0) {
+      perror("Error receiving serial number");
+      exit(EXIT_FAILURE);
+    }
+    serial_number_str[recv_bytes] = '\0';
+
+    printf("Received serial number: %s\n", serial_number_str);
+
+    close(sockfd);
+    return 0;
+}
+
--- CSCB09 - Soft. Tools & Sys. Prog/A4/hmu-server.c
+++ CSCB09 - Soft. Tools & Sys. Prog/A4/hmu-server.c
@@ -10,9 +10,154 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_BUFFER_SIZE 1024
+
+void handle_client(int client_sock) {
+  char buffer[MAX_BUFFER_SIZE];
+  char user_name[9]; // 8 letters or digits + '\0'
+  char file_name[101]; // 100 bytes + '\0'
+  char file_size_str[11]; // 10 digits for file size + '\0'
+
+  // Receive username from client //
+  int recv_bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+  if (recv_bytes <= 0) {
+    close(client_sock);
+    return;
+  }
+  buffer[recv_bytes] = '\0';
+  sscanf(buffer, "%8[^\n]", user_name);
+
+  // Receive filename from client //
+  recv_bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+  if (recv_bytes <= 0) {
+    close(client_sock);
+    return;
+  }
+  buffer[recv_bytes] = '\0';
+  sscanf(buffer, "%100[^/\n]", file_name);
+
+  // Receive file size from client //
+  recv_bytes = recv(client_sock, buffer, sizeof(buffer) - 1, 0);
+  if (recv_bytes <= 0) {
+    close(client_sock);
+    return;
+  }
+  buffer[recv_bytes] = '\0';
+  sscanf(buffer, "%10[^\n]", file_size_str);
+  long file_size = atol(file_size_str);
+
+  // Receive file content from client //
+  FILE *file = fopen(file_name, "wb");
+  if (!file) {
+    send(client_sock, "HDERR\n", 6, 0);
+    close(client_sock);
+    return;
+  }
+
+  long total_received = 0;
+  while (total_received < file_size) {
+      recv_bytes = recv(client_sock, buffer, sizeof(buffer), 0);
+      if (recv_bytes <= 0) {
+        fclose(file);
+        close(client_sock);
+        return;
+      }
+      total_received += recv_bytes;
+      fwrite(buffer, 1, recv_bytes, file);
+  }
+  fclose(file);
+
+  // Send serial number to client //
+  static int serial_number = 0;
+  char serial_number_str[11]; // 10 digits for serial number + '\0'
+  snprintf(serial_number_str, sizeof(serial_number_str), "%d", serial_number++);
+  send(client_sock, serial_number_str, strlen(serial_number_str), 0);
+  
+  close(client_sock);
+}
+
+void sigchld_handler(int signum) {
+  while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 // cmdline reminder: portnum, helper
 int main(int argc, char *argv[]) {
-  // TODO
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <port_number> <helper_program_pathname>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int port_number = atoi(argv[1]);
+  const char *helper_program_pathname = argv[2];
+
+  struct sigaction sa;
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("Error setting signal handler");
+    exit(EXIT_FAILURE);
+  }
+
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    perror("Error opening socket");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port_number);
+
+  int optval = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) == -1) {
+    perror("Error setting socket options");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+    perror("Error binding socket");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(sockfd, SOMAXCONN) == -1) {
+    perror("Error listening on socket");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server is listening on port %d...\n", port_number);
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    int client_sock = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_sock == -1) {
+      continue;
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+      perror("Error forking a child process");
+      exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+      // Child process //
+      close(sockfd);
+      execlp(helper_program_pathname, helper_program_pathname, (char *)NULL);
+      perror("Error executing helper program");
+      exit(EXIT_FAILURE);
+    } else {
+      // Parent process //
+      close(client_sock);
+    }
+  }
+
+  close(sockfd);
   return 0;
 }
+
